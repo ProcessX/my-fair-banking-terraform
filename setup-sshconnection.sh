9588f675d5a2f605725ce065a195702dd23cd7c8
ProcessX <luc2526@gmail.com>
2024-05-18 16:17:25 +0200

    infrastructure created

--- setup-sshconnection.sh
+++ setup-sshconnection.sh
@@ -1,8 +1,1 @@
 #! /bin/bash
-sudo apt-get update
-sudo apt -y install git-all
-
-
-echo ${ssh_key} >> ~/.ssh/authorized_keys
-chmod -R go= ~/.ssh
-chown -R azureuser:azureuser ~/.ssh
--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 sudo apt-get update
 sudo apt -y install git-all
-sudo chmod 600 /home/azureuser/.ssh/id_rsa
-echo ${ssh_key} > /home/azureuser/.ssh/id_rsa
 sudo apt -y install ansible
 sudo chmod -R 700 /home/azureuser/.ansible
 sudo chown -R azureuser:azureuser /home/azureuser/.ansible
